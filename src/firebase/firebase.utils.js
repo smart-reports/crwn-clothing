74b9383f468f2b6db987782691ee9d9d52bebc6b
smart-reports <50381700+smart-reports@users.noreply.github.com>
2020-07-09 18:58:35 -0500

    created user and cart sagas for signin signup signout gooogle and email

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,16 +3,15 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-  apiKey: "AIzaSyAHEcwj1v6QscFYTlVjQo5C230doGWS6W0",
-  authDomain: "crwn-db-1e692.firebaseapp.com",
-  databaseURL: "https://crwn-db-1e692.firebaseio.com",
-  projectId: "crwn-db-1e692",
-  storageBucket: "crwn-db-1e692.appspot.com",
-  messagingSenderId: "897033540768",
-  appId: "1:897033540768:web:25a673508b008dfaf665c5",
-  measurementId: "G-04LPMFW4QK"
-};
-
+    apiKey: "AIzaSyAHEcwj1v6QscFYTlVjQo5C230doGWS6W0",
+    authDomain: "crwn-db-1e692.firebaseapp.com",
+    databaseURL: "https://crwn-db-1e692.firebaseio.com",
+    projectId: "crwn-db-1e692",
+    storageBucket: "crwn-db-1e692.appspot.com",
+    messagingSenderId: "897033540768",
+    appId: "1:897033540768:web:25a673508b008dfaf665c5",
+    measurementId: "G-04LPMFW4QK"
+  };
 firebase.initializeApp(config);
 
 export const createUserProfileDocument = async (userAuth, additionalData) => {
@@ -73,11 +72,20 @@ export const convertCollectionsSnapshotToMap = collections => {
   }, {});
 };
 
+export const getCurrentUser = () => {
+  return new Promise((resolve, reject) => {
+    const unsubscribe = auth.onAuthStateChanged(userAuth => {
+      unsubscribe();
+      resolve(userAuth);
+    }, reject);
+  });
+};
+
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
-const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({ prompt: 'select_account' });
-export const signInWithGoogle = () => auth.signInWithPopup(provider);
+export const googleProvider = new firebase.auth.GoogleAuthProvider();
+googleProvider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(googleProvider);
 
 export default firebase;
